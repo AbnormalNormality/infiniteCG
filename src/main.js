ad025005ff8b638534d00e6d5713396faed674cc
Alia Driscoll <dan.driscoll@aussiebb.com.au>
2025-05-06 10:23:35 +1000

    Added card descriptions, enemy hp, player hp and energy, updated enemy initiation, energy checks, removed some log functions, player ui (hp and energy), and card interaction

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ const presets = {
       background: "#fdd",
       id: 0,
       type: "card",
+      description: "Debug description",
     },
     1: {
       name: "Debug Card 2",
@@ -13,6 +14,7 @@ const presets = {
       background: "#fdf",
       id: 1,
       type: "card",
+      description: "Looooooooooooooooooooooooooooooooong description",
     },
     2: {
       name: "Debug Card 3",
@@ -20,6 +22,12 @@ const presets = {
       background: "#ddf",
       id: 2,
       type: "card",
+      description: `Multiple
+rows,
+like,
+more
+than
+3`,
     },
   },
   enemies: {
@@ -29,7 +37,8 @@ const presets = {
       background: "#dfd",
       id: 0,
       type: "enemy",
-      hp: 50,
+      maxHp: 50,
+      hp: 0,
     },
     1: {
       name: "Debug Enemy 2",
@@ -37,7 +46,8 @@ const presets = {
       background: "#ddf",
       id: 1,
       type: "enemy",
-      hp: 50,
+      maxHp: 69,
+      hp: 0,
     },
     2: {
       name: "Debug Enemy 3",
@@ -45,7 +55,8 @@ const presets = {
       background: "#dff",
       id: 2,
       type: "enemy",
-      hp: 50,
+      maxHp: 42,
+      hp: 0,
     },
   },
 };
@@ -54,6 +65,10 @@ const player = {
   hand: [],
   deck: [],
   discardPile: [],
+  maxHp: 50,
+  hp: 0,
+  energy: 0,
+  turnEnergy: 3,
 };
 
 player.discardPile.push(presets.cards[0]);
@@ -69,9 +84,10 @@ const combat = {
   enemies: [],
 };
 
-combat.enemies.push(presets.enemies[0]);
-combat.enemies.push(presets.enemies[1]);
-combat.enemies.push(presets.enemies[2]);
+addEnemy(presets.enemies[0]);
+addEnemy(presets.enemies[1]);
+addEnemy(presets.enemies[2]);
+addEnemy(presets.enemies[2]);
 
 const hand = document.getElementById("hand");
 const enemies = document.getElementById("enemies");
@@ -122,6 +138,10 @@ let offsetX = 0;
 let offsetY = 0;
 
 function onMouseDown(event) {
+  if (player.energy <= 0) {
+    return;
+  }
+
   const original = event.currentTarget;
   const rect = original.getBoundingClientRect();
 
@@ -189,8 +209,7 @@ function onMouseUp(event) {
     }
 
     if (targetEnemyData.length > 0) {
-      console.log("Dragged card:", draggedCardData);
-      console.log("Enemy data:", targetEnemyData);
+      cardInteraction(draggedCardData, targetEnemyData);
     }
 
     draggedCard.remove();
@@ -219,8 +238,13 @@ function renderHand() {
     cardImage.src = card.image;
     cardImage.draggable = false;
 
+    const cardDescription = document.createElement("div");
+    cardDescription.className = "cardDescription";
+    cardDescription.innerText = card.description;
+
     cardElement.appendChild(cardName);
     cardElement.appendChild(cardImage);
+    cardElement.appendChild(cardDescription);
     cardElement.addEventListener("mousedown", onMouseDown);
     hand.appendChild(cardElement);
   });
@@ -247,12 +271,15 @@ function renderEnemies() {
     const enemyHp = document.createElement("progress");
     enemyHp.className = "enemyHp";
     enemyHp.value = enemy.hp;
-    enemyHp.max = enemy.hp;
+    enemyHp.max = enemy.maxHp;
 
     const hpText = document.createElement("span");
     hpText.className = "enemyHpValue";
-    const percentage = Math.round((enemy.hp / enemyHp.max) * 100);
-    hpText.innerText = `${percentage}%`;
+    let percentage = Math.floor((enemy.hp / enemy.maxHp) * 100);
+    if (percentage === 0 && enemy.hp > 1) {
+      percentage = 1;
+    }
+    hpText.innerText = `${enemy.hp} / ${enemy.maxHp} (${percentage}%)`;
 
     enemyElement.appendChild(enemyName);
     enemyElement.appendChild(enemyImage);
@@ -263,13 +290,14 @@ function renderEnemies() {
   });
 }
 
-// Game flow functions
 function updateDisplay() {
   renderHand();
   renderEnemies();
+  updatePlayerUi();
 }
 
 function startTurn() {
+  player.energy = player.turnEnergy;
   draw(5);
   updateDisplay();
 }
@@ -299,6 +327,49 @@ function shuffle(array) {
     [array[i], array[j]] = [array[j], array[i]];
   }
 }
+
+function addEnemy(enemy) {
+  enemy = Object.assign({}, enemy);
+
+  enemy.hp = enemy.maxHp;
+  combat.enemies.push(enemy);
+}
+
+function cardInteraction(card, targets) {
+  if (player.energy <= 0) {
+    return;
+  }
+  player.energy--;
+
+  targets.forEach((enemy) => {
+    modifyEnemyHp(enemy, -1);
+  });
+
+  updateDisplay();
+}
+
+function modifyEnemyHp(enemy, value) {
+  enemy.hp = Math.max(0, enemy.hp + value);
+}
+
+function initPlayer() {
+  player.hp = player.maxHp;
+}
+
+const playerHp = document.getElementById("playerHp");
+const playerEnergy = document.getElementById("playerEnergy");
+
+function updatePlayerUi() {
+  playerHp.value = player.hp;
+  playerHp.max = player.maxHp;
+
+  playerEnergy.value = player.energy;
+  playerEnergy.max = player.turnEnergy;
+
+  // playerEnergy.textContent = `${player.energy} / ${player.turnEnergy} Energy`;
+}
+
+initPlayer();
 
 endTurnButton.onclick = endTurn;
 
