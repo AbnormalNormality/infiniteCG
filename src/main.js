604f8c41d0703861b2b4e6dfb78057e52dd1d45e
Alia Driscoll <dan.driscoll@aussiebb.com.au>
2025-05-08 16:57:35 +1000

    Moved some content from game.js to main.js

---
+++ src/main.js
@@ -0,0 +1,81 @@
+function clearCacheAndReload() {
+  if ("caches" in window) {
+    caches
+      .keys()
+      .then(function (names) {
+        for (let name of names) {
+          caches.delete(name);
+        }
+      })
+      .then(function () {
+        window.location.reload(true);
+      });
+  } else {
+    window.location.reload(true);
+  }
+}
+
+function handleKeyRelease(event) {
+  if (event.altKey && event.code === "KeyR") {
+    clearCacheAndReload();
+  } else if (event.altKey && event.code === "KeyG") {
+    window.open("https://github.com/AbnormalNormality/InfiniteCG", "_blank");
+  } else if (event.altKey && event.code === "KeyC") {
+    const basePath =
+      window.location.origin + window.location.pathname.replace(/\/[^/]*$/, "");
+    const creditsLink = basePath + "/pages/credits.html";
+    window.open(creditsLink, "_blank");
+  }
+}
+
+document.addEventListener("keyup", handleKeyRelease);
+
+//
+
+function startMessage() {
+  const basePath =
+    window.location.origin + window.location.pathname.replace(/\/[^/]*$/, "");
+  const creditsLink = basePath + "/pages/credits.html";
+
+  console.log(
+    "Thank you for playing %cInfiniteCG%c!\n" +
+      "Alt+R to clear cache/reload\n" +
+      "Alt+G to open the game's GitHub repository\n" +
+      "Alt+C to view the credits: " +
+      creditsLink,
+    "color: #090",
+    "color: unset"
+  );
+}
+
+function checkForUpdates() {
+  const lastUpdate = localStorage.getItem("lastUpdate");
+
+  const repoUrl =
+    "https://api.github.com/repos/abnormalnormality/infinitecg/commits/main";
+
+  fetch(repoUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      if (!Object.keys(data).includes("sha")) {
+        console.error("Invalid response from GitHub API");
+        return;
+      }
+      const currentUpdate = data.sha.substring(0, 7);
+
+      if (!lastUpdate) {
+        localStorage.setItem("lastUpdate", currentUpdate);
+      } else if (lastUpdate !== currentUpdate) {
+        if (confirm("A new update is available. Do you want to reload?")) {
+          localStorage.setItem("lastUpdate", currentUpdate);
+          clearCacheAndReload();
+        }
+      }
+    })
+    .catch((error) => {
+      console.error("Could not check for updates:", error);
+    });
+}
+
+startMessage();
+checkForUpdates();
